0028f97b17599a44a9b34f9aea9cd36e2346fbc1
Enrique Sotomayor <sotomaque@gmail.com>
2022-11-27 17:48:41 -1000

    http server request logic

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -21,6 +23,12 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            // create Request from buffer using try_from (see src/http/request.rs)
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                },
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
